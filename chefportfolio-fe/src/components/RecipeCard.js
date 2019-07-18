90a090e7e1005b22f45c916a69daf919aeb27c2d
Kathy Cuevas <meetnerdykat@gmail.com>
2019-07-18 04:55:17 -0400

    Added responsiveness to the home slash landing page...will add responsiveness to navbar as well. After completeting the Dashboard component of cours.

--- chefportfolio-fe/src/components/RecipeCard.js
+++ chefportfolio-fe/src/components/RecipeCard.js
@@ -2,15 +2,61 @@ import React from 'react';
 
 const Recipe = props => {
   return (
-    <div className="cards">
-      <img src={props.picture} alt={props.name} />
-
-      <div className="card-info">
-        <p className="username">{props.name}</p>
-        <h2 className="title">{props.name}</h2>
-        <p className="description">{props.description}</p>
+    <div className="container">
+      <div className="row">
+        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+          <div className="card mb-5">
+            <img src={props.picture} alt={props.name} />
+            <div className="card-body">
+              <h3 className="card-title">{props.name}</h3>
+              <h4 className="card-subtitle mb-2 text-muted">
+                {props.name}
+              </h4>
+              <p className="card-text">
+                {props.description}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+          <div className="card mb-5">
+            <img src={props.picture} alt={props.name} />
+            <div className="card-body">
+              <h3 className="card-title">{props.name}</h3>
+              <h4 className="card-subtitle mb-2 text-muted">
+                {props.name}
+              </h4>
+              <p className="card-text">
+                {props.description}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+          <div className="card mb-5">
+            <img src={props.picture} alt={props.name} />
+            <div className="card-body">
+              <h3 className="card-title">{props.name}</h3>
+              <h4 className="card-subtitle mb-2 text-muted">
+                {props.name}
+              </h4>
+              <p className="card-text">
+                {props.description}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+
+    // <div className="cards">
+    //   <img src={props.picture} alt={props.name} />
+    //   <div className="card-info">
+    //     <p className="username">{props.name}</p>
+    //     <h2 className="title">{props.name}</h2>
+    //     <p className="description">{props.description}</p>
+    //   </div>
+    // </div>
   );
 };
 
